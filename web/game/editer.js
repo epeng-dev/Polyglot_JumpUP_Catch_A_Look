4196ddb0153e85ebc7e7e022bbd0a9fdb7d38c99
back_gwang_min <back5373122@gmail.com>
2018-01-04 14:26:10 +0900

    hidden action add

--- web/game/editer.js
+++ web/game/editer.js
@@ -5,41 +5,40 @@ var width = 20;
 var mode;
 
 
-function all_clear(){
+function all_clear() {
     var clear_cheak = confirm("정말 지우시겠습니까?");
-    if(clear_cheak == true){
+    if (clear_cheak == true) {
         canvas.clear();
         console.log("전체 지우기 실행");
-    }
-    else{
+    } else {
         console.log("전체 지우기 취소");
     }
-    
+
 };
 
-$(function () {
-    
+$(function() {
+
     canvas = window._canvas = new fabric.Canvas('c');
-    canvas.isDrawingMode= 1;
+    canvas.isDrawingMode = 1;
     canvas.freeDrawingBrush.width = 20;
-    
-    document.getElementById('pick').addEventListener('click', function (e) {
+
+    document.getElementById('pick').addEventListener('click', function(e) {
         target = e.target.id;
-        if(target != "pick"){
+        if (target != "pick") {
             $('#choose_color').val(target);
-            console.log("색 해쉬 코드 : "+target);
+            console.log("색 해쉬 코드 : " + target);
             canvas.freeDrawingBrush.color = target;
         }
     });
-    $('#choose_color').change(function(){
+    $('#choose_color').change(function() {
         target = $('#choose_color').val();
-        console.log("색 해쉬 코드 : "+target);
+        console.log("색 해쉬 코드 : " + target);
         canvas.freeDrawingBrush.color = target;
     });
-    $(".canvas-container").mouseup(function(e) {
+    $("#owner_drawing").mouseup(function(e) {
         console.log("마우스 떼면 전송");
         setTimeout("trsvg_fun()", 100);
-        
+
     });
 });
 /*
